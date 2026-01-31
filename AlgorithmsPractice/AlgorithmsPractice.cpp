574929f7f25b4423a8ef51baaa8157dde5591699
Mark <75143899+qlemon-56@users.noreply.github.com>
2026-01-31 11:47:34 +0000

    Add project files.

---
+++ AlgorithmsPractice/AlgorithmsPractice.cpp
@@ -0,0 +1,12 @@
+﻿// AlgorithmsPractice.cpp : Defines the entry point for the application.
+//
+
+#include "AlgorithmsPractice.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
