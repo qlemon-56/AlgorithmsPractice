3091cb0d52300bea22c1c459e5669876eb35ddb5
Mark <75143899+qlemon-56@users.noreply.github.com>
2026-02-03 17:20:12 +0000

    Reviewed permutations.cpp; learnt more about backtracking; attempted to solve chessBoard problem

--- AlgorithmsPractice/AlgorithmsPractice.cpp
+++ AlgorithmsPractice/AlgorithmsPractice.cpp
@@ -26,10 +26,41 @@ Some common data structures
 
 */
 
+array <int, 4> column = {0, 0, 0,0 };
+
+array <int, 4> diag1 = {0, 0, 0,0 };
+
+array <int, 4> diag2 = {0, 0, 0,0 };
+
+int myCount = 0;
+void chessBoard(int y) {
+
+	if (y == 4) {
+		myCount++;
+		return;
+	}
+	else {
+
+		for (int x = 0; x < 4; x++) {
+
+			if (column[x] || diag1[x + y] || diag2[x - y + 4 - 1]) continue;
+
+			column[x] = diag1[x + y] = diag2[x - y + 4 - 1] = 1;
+			chessBoard(y + 1);
+			column[x] = diag1[x + y] = diag2[x - y + 4 - 1] = 0;
+
+		}
+	}
+}
 
 int main() {
+
+	// begin by checking previous knowledge
+	// yesterday we made a function to create permutations of a list from 0 to n - 1 e.g. {0, 1, 2, 3, 4} 
+	// this worked through recursion
 	
-	
+	chessBoard(4);
+
 
 	return 0;
 }
--- AlgorithmsPractice/Algos/permutations.cpp
+++ AlgorithmsPractice/Algos/permutations.cpp
@@ -1,11 +1,13 @@
 
 #include <iostream>
 #include <vector>
+#include <array>
 
 using namespace std;
 
-vector <int> permutation;
+/*
 
+vector <int> permutation;
 for (int i = 0; i < 6; i++) {
 	permutation.push_back(i);
 }
@@ -16,17 +18,16 @@ do {
 	}
 	cout << endl;
 } while (next_permutation(permutation.begin(), permutation.end()));
+*/
 
-/*
 array<bool, 3> chosen = {false, false, false};
-vector <int> permutation;
+vector <int> Permutation;
 
 // set would be {0 , 1, 2 }
+void search() {
+	if (Permutation.size() == 3) {
 
-void  search() {
-	if (permutation.size() == 3) {
-
-		for (auto y : permutation) {
+		for (auto y : Permutation) {
 			cout << y;
 		}
 		cout << endl;
@@ -38,13 +39,50 @@ void  search() {
 			if (chosen[i]) continue;
 
 			chosen[i] = true;
-			permutation.push_back(i);
+			Permutation.push_back(i);
 			search();
 			chosen[i] = false;
-			permutation.pop_back();
+			Permutation.pop_back();
 
 		}
 	}
 }
+
+/*
+This problem in chapter 5 of Guide to Competitive Programming introduced me to the concept of backtracking
+
+The problem can be visualised using a State Space Tree; when we have a problem which requires:
+1. A State Space Tree (SST) and
+2. All possible solutions
+it indicates we are in the presence of a backtracking problem. 
+
+The three keys of backtracking are:
+1. Choices
+2. Constraints
+3. Goal
+
+Here the choice is to use one number at a time, the constraint is using it only once, and the goal is to create a permutation
+The backtracking formula is in this type of function
+
+void Backtrack(res, args) {
+	
+	if (goal_reached) {
+		// process solution
+		add solution to res (result)
+
+		return
+	} else {
+		
+		for (int i = 0; i < number_of_choices; i++) {
+			if (choices[i] is valid // valid does not equal true in this case ) {
+				make choices[i] // add to used list and update ur solution
+				Backtrack(res, args)
+				undo choices[i] // remove from used list and delete ur solution
+			}
+		}
+	}
+}
+
 */
+
 
